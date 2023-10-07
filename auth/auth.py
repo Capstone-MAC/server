866a1ce17b7f2806d2378035809436f6f77d8d77
rainbow5079 <rlaxogh507906@gmail.com>
2023-10-07 17:45:05 +0900

    Provide API KEY

--- auth/auth.py
+++ auth/auth.py
@@ -1,9 +1,7 @@
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security
 from typing import Optional
-# from auth.env import API_KEY
-
-API_KEY = "test-key"
+from auth.env import API_KEY
 
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
